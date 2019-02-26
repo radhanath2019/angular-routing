0ac9c0dda0d290215b6a594d9666f1387fffd8e4
unknown <radhanathparida1626@example.com>
2019-02-26 11:36:05 +0530

    login added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{DashboardComponent} from './dashboard/dashboard.component';
+import{LoginComponent} from './login/login.component';
+import{LogoutComponent} from './logout/logout.component';
+import{HomePageComponent} from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"login",
+    component:LoginComponent,
+  },
+  {
+  path:"logout",
+  component:LogoutComponent,
+ },
+ {
+  path:"dashboard",
+  component:DashboardComponent,
+ },
+ {
+  path:"homePage",
+  component:HomePageComponent,
+ },
+
+ {
+   path:"",
+   redirectTo:"/login",
+   pathMatch:"full",
+
+ }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginComponent,
+    LogoutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
